751255c4993ee3165b0838f8aaf46c3a89df566c
Tristan Grovender <46428421+tristangrovender@users.noreply.github.com>
2021-10-30 13:41:36 -0500

    Working with portals

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -3,7 +3,6 @@ import React, { useState } from "react";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
 import ErrorModal from "../UI/ErrorModal";
-import Wrapper from "../Helpers/Wrapper";
 import classes from "./AddUser.module.css";
 
 const AddUser = props => {
@@ -45,7 +44,7 @@ const AddUser = props => {
   };
 
   return (
-    <Wrapper>
+    <>
       {error && (
         <ErrorModal
           title={error.title}
@@ -72,7 +71,7 @@ const AddUser = props => {
           <Button type="submit">Add User</Button>
         </form>
       </Card>
-    </Wrapper>
+    </>
   );
 };
 
